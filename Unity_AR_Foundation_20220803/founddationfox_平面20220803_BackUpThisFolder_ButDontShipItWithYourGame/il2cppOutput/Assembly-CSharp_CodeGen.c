175d0b9bed70714e1f84d2f840170d90c905226b
EFLEPFE <105775244+EFLEPFE@users.noreply.github.com>
2022-08-03 16:28:18 +0800

    平面辨識 完成 20220803

---
+++ Unity_AR_Foundation_20220803/founddationfox_平面20220803_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,57 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void neat.TapToSpawnObject::Awake()
+extern void TapToSpawnObject_Awake_mC2CA4B7E4A9C9A8624754FF4EA74A157A3E77E8F (void);
+// 0x00000002 System.Void neat.TapToSpawnObject::Update()
+extern void TapToSpawnObject_Update_m62FDF29C61D6E9B22F4AAE4CC17DEB76F9DD296E (void);
+// 0x00000003 System.Void neat.TapToSpawnObject::TapAndSpawn()
+extern void TapToSpawnObject_TapAndSpawn_m26E3B3B65F791EA45764B1C0D4E071EAD5894F8D (void);
+// 0x00000004 System.Void neat.TapToSpawnObject::.ctor()
+extern void TapToSpawnObject__ctor_m59D72D1EBE654CECB4A49B4B53177A1EEF13DCD6 (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	TapToSpawnObject_Awake_mC2CA4B7E4A9C9A8624754FF4EA74A157A3E77E8F,
+	TapToSpawnObject_Update_m62FDF29C61D6E9B22F4AAE4CC17DEB76F9DD296E,
+	TapToSpawnObject_TapAndSpawn_m26E3B3B65F791EA45764B1C0D4E071EAD5894F8D,
+	TapToSpawnObject__ctor_m59D72D1EBE654CECB4A49B4B53177A1EEF13DCD6,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	2086,
+	2086,
+	2086,
+	2086,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
